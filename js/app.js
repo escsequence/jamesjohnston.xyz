457959a46ef2f2f592954873fec4e063b1b7f294
James Johnston <jamestheprogrammer@outlook.com>
2021-05-05 15:51:07 -0500

    Fixed a bug with scrolling javascript not being loaded properly. Adding blog example code for the future.

--- js/app.js
+++ js/app.js
@@ -25,6 +25,40 @@ function($scope) {
 app.controller('indexController', ['$scope',
 function($scope) {
   console.log("Index page!")
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+      if (
+          location.pathname.replace(/^\//, "") ==
+              this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+      ) {
+          var target = $(this.hash);
+          target = target.length
+              ? target
+              : $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+              $("html, body").animate(
+                  {
+                      scrollTop: target.offset().top,
+                  },
+                  1000,
+                  "easeInOutExpo"
+              );
+              return false;
+          }
+      }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+      $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+      target: "#sideNav",
+  });
 }]);
 
 app.controller('blogController', ['$scope',
