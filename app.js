1aa53036cd95f37d1e1ed49898909465e8702fda
James Johnston <jamestheprogrammer@outlook.com>
2021-05-05 12:25:43 -0500

    Nodejs change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('NodeJS server running on Shared Hosting\n');
+  res.end('Change has been made.');
 });
 
 server.listen(port, hostname, () => {
